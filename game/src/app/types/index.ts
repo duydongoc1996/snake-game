462c051ca91109b33ccd99f1f230d3b2ef16d208
Shira <duydongoc1996@gmail.com>
2024-07-06 13:53:24 +0700

    prototype for GameState

--- game/src/app/types/index.ts
+++ game/src/app/types/index.ts
@@ -1,1 +1,66 @@
-export type GameState = {};
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export enum Direction {
+  UP = "UP",
+  DOWN = "DOWN",
+  LEFT = "LEFT",
+  RIGHT = "RIGHT",
+}
+
+export enum GameStatus {
+  INIT = "INIT",
+  PLAYING = "PLAYING",
+  WIN = "WIN",
+  LOSE = "LOSE",
+}
+
+export enum GameCellType {
+  EMPTY = "EMPTY",
+  BODY = "BODY",
+  HEAD = "HEAD",
+  TAIL = "TAIL",
+  BAIT = "BAIT",
+}
+
+export type GameTimer = {
+  step: number;
+  start: Date;
+  interval: number | null;
+};
+
+export type GameCell = {
+  type: GameCellType;
+};
+
+export type GameMatrix = {
+  height: number;
+  width: number;
+
+  objects: Array<Array<GameCell>>;
+};
+
+export type GameSnake = {
+  length: number;
+  head: Position;
+  tail: Position;
+  direction: Direction;
+};
+
+export type GameBait = {
+  position: Position;
+};
+
+export type GameState = {
+  status: GameStatus;
+
+  timer: GameTimer;
+
+  matrix: GameMatrix;
+
+  snake: GameSnake;
+
+  bait: GameBait;
+};
