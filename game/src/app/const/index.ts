3d6f43fae9806703791db13dc9388e46a648a4ce
Shira <duydongoc1996@gmail.com>
2024-07-07 14:39:17 +0700

    add config width, height

--- game/src/app/const/index.ts
+++ game/src/app/const/index.ts
@@ -2,3 +2,8 @@ export const DEFAULT_MATRIX_HEIGHT = 3;
 export const DEFAULT_MATRIX_WIDTH = 3;
 export const DEFAULT_SNAKE_LENGTH = 3;
 export const FRAME_TICK = 1000;
+
+export const MIN_MATRIX_HEIGHT = 3;
+export const MIN_MATRIX_WIDTH = 3;
+export const MAX_MATRIX_HEIGHT = 20;
+export const MAX_MATRIX_WIDTH = 20;
